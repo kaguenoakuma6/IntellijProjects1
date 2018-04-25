1f3d6163e1ec153c0049850f403e928c7c1c81bc
shiro <shadowfocker666@gmail.com>
2018-04-25 18:54:25 -0500

    Upload Class modules

--- src/com/intellij/Polymorphism/Vehicle.java
+++ src/com/intellij/Polymorphism/Vehicle.java
@@ -1,7 +1,63 @@
 package com.intellij.Polymorphism;
 
-public class Vehicle
+class Vehicle
 {
+    class car
+    {
+        private boolean engine;
+        private int weels, cylinders;
+        private String name;
+
+        public car(int cylinders, String name)
+        {
+            this.cylinders = cylinders;
+            this.name = name;
+            this.weels = 4;
+            this.engine = true;
+        }
+
+        public String startEngine()
+        {
+            return "No Start Engine";
+        }
+
+        public String accelerate()
+        {
+            return "No Accelerate";
+        }
+
+        public String Break()
+        {
+            return "Breaking car";
+        }
+    }
+
+    class Charger extends car
+    {
+        public Charger()
+        {
+            super(4, "Charger");
+        }
+
+        @Override
+        public String startEngine()
+        {
+            return "Start Engine Charger";
+        }
+
+        @Override
+        public String accelerate()
+        {
+            return "Accelerate Charger";
+        }
+
+        @Override
+        public String Break()
+        {
+            return "Charger is breaking";
+        }
+    }
+
     public static void main(String [] Args)
     {
 
