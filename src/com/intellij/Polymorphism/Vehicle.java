c8df92adbba672aed2025cc290adec5b99092213
kaguenoakuma6 <31392878+kaguenoakuma6@users.noreply.github.com>
2018-04-25 20:59:48 -0500

    Complement to Vehicle class

--- src/com/intellij/Polymorphism/Vehicle.java
+++ src/com/intellij/Polymorphism/Vehicle.java
@@ -1,63 +1,71 @@
 package com.intellij.Polymorphism;
 
+class car
+{
+    private boolean engine;
+    private int weels, cylinders;
+    private String name;
+
+    public car(int cylinders, String name)
+    {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.weels = 4;
+        this.engine = true;
+    }
+
+    public String startEngine()
+    {
+        return "No Start Engine";
+    }
+
+    public String accelerate()
+    {
+        return "No Accelerate";
+    }
+
+    public String Break()
+    {
+        return "Breaking car";
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Charger extends car
+{
+    public Charger()
+    {
+        super(4, "Charger");
+    }
+
+    @Override
+    public String startEngine()
+    {
+        return "Start Engine Charger";
+    }
+
+    @Override
+    public String accelerate()
+    {
+        return "Accelerate Charger";
+    }
+
+    @Override
+    public String Break()
+    {
+        return "Charger is breaking";
+    }
+}
+
 class Vehicle
 {
-    class car
-    {
-        private boolean engine;
-        private int weels, cylinders;
-        private String name;
-
-        public car(int cylinders, String name)
-        {
-            this.cylinders = cylinders;
-            this.name = name;
-            this.weels = 4;
-            this.engine = true;
-        }
-
-        public String startEngine()
-        {
-            return "No Start Engine";
-        }
-
-        public String accelerate()
-        {
-            return "No Accelerate";
-        }
-
-        public String Break()
-        {
-            return "Breaking car";
-        }
-    }
-
-    class Charger extends car
-    {
-        public Charger()
-        {
-            super(4, "Charger");
-        }
-
-        @Override
-        public String startEngine()
-        {
-            return "Start Engine Charger";
-        }
-
-        @Override
-        public String accelerate()
-        {
-            return "Accelerate Charger";
-        }
-
-        @Override
-        public String Break()
-        {
-            return "Charger is breaking";
-        }
-    }
-
     public static void main(String [] Args)
     {
 
