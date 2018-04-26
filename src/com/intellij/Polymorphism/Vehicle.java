4a2027e469ff3d1358f04116542b292adadb257e
kaguenoakuma6 <31392878+kaguenoakuma6@users.noreply.github.com>
2018-04-25 21:28:32 -0500

    Complement to Vehicle class

--- src/com/intellij/Polymorphism/Vehicle.java
+++ src/com/intellij/Polymorphism/Vehicle.java
@@ -1,12 +1,12 @@
 package com.intellij.Polymorphism;
 
-class car
+class Car
 {
     private boolean engine;
     private int weels, cylinders;
     private String name;
 
-    public car(int cylinders, String name)
+    public Car(int cylinders, String name)
     {
         this.cylinders = cylinders;
         this.name = name;
@@ -29,16 +29,28 @@ class car
         return "Breaking car";
     }
 
-    public int getCylinders() {
+    public int getCylinders()
+    {
         return cylinders;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
+    }
+
+    public boolean getEngine()
+    {
+        return engine;
+    }
+
+    public int getWeels()
+    {
+        return weels;
     }
 }
 
-class Charger extends car
+class Charger extends Car
 {
     public Charger()
     {
@@ -64,10 +76,77 @@ class Charger extends car
     }
 }
 
-class Vehicle
+class Ferrari extends Car
+{
+    public Ferrari()
+    {
+        super(8, "Ferrary");
+    }
+
+    @Override
+    public String startEngine()
+    {
+        return "Ferrari Engine Start";
+    }
+
+    @Override
+    public String Break()
+    {
+        return "Ferrari Break";
+    }
+}
+
+class Mercedes extends Car
+{
+    public Mercedes()
+    {
+        super(6, "Mercedes Benz");
+    }
+
+    @Override
+    public String accelerate()
+    {
+        return "Mercedes is Accelerate";
+    }
+
+    @Override
+    public String Break()
+    {
+        return "Mercedes is Break";
+    }
+}
+
+
+
+public class Vehicle
 {
     public static void main(String [] Args)
     {
+        for (int a = 0;  a < 10; a++)
+        {
+            Car car = randCar();
 
+            System.out.println("Car Name: " + car.getName() + "\nCar Cylinder: " + car.getCylinders() + "\nCar Wheels: " + car.getWeels() + "\nCar Engine: " + car.getEngine()
+            + "\n" + car.startEngine() + "\n" + car.accelerate() + "\n" + car.Break() + "\n");
+        }
+    }
+
+    public static Car randCar()
+    {
+        int rNum = (int) (Math.random() * 3 ) + 1;
+        System.out.println("Generated Number: " + rNum);
+
+        switch (rNum)
+        {
+            case 1:
+                return new Charger();
+
+            case 2:
+                return new Ferrari();
+
+            case 3:
+                return new Mercedes();
+        }
+        return null;
     }
 }
