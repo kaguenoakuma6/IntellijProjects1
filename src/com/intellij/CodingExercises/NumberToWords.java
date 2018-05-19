c70212a36678f8eda44c3aab486a8ef752d73504
shiro <shadowfocker666@gmail.com>
2018-05-18 19:16:36 -0500

    Chalenges 18 - 05 - 2018

--- src/com/intellij/CodingExercises/NumberToWords.java
+++ src/com/intellij/CodingExercises/NumberToWords.java
@@ -7,7 +7,16 @@ public class NumberToWords
         System.out.println(getDigitCount(10));
         System.out.println(getDigitCount(0));
         System.out.println(getDigitCount(5));
-        System.out.println(getDigitCount(123456789));
+        System.out.println(getDigitCount(1234589));
+
+        System.out.println(reverse(-2));
+        System.out.println(reverse(-508));
+
+
+        numberToWords(123);
+        numberToWords(1010);
+        numberToWords(1000);
+        numberToWords(-12);
     }
 
     public static void numberToWords(int number)
@@ -18,11 +27,11 @@ public class NumberToWords
         }
         else
         {
-            int reversed = 0, leng = 0, leng2 = 0;
-            reversed = reverseNumber(number);
+            int reversed = 0, lengNum = 0, lengReversed = 0;
+            reversed = reverse(number);
             String numberWord = "";
-            leng = getDigitCount(number);
-            leng2 = getDigitCount(reversed);
+            lengNum = getDigitCount(number);
+            lengReversed = getDigitCount(reversed);
 
             while ( reversed > 0 )
             {
@@ -30,26 +39,89 @@ public class NumberToWords
 
                 switch (digito)
                 {
+                    case 0:
+                        numberWord += "Zero";
+                        break;
+
                     case 1:
-                        numberWord += "ONE";
+                        numberWord += "One";
+                        break;
+
+                    case 2:
+                        numberWord += "Two";
+                        break;
+
+                    case 3:
+                        numberWord += "Three";
+                        break;
+
+                    case 4:
+                        numberWord += "Four";
+                        break;
+
+                    case 5:
+                        numberWord += "Five";
+                        break;
+
+                    case 6:
+                        numberWord += "Six";
+                        break;
+
+                    case 7:
+                        numberWord += "Seven";
+                        break;
+
+                    case 8:
+                        numberWord += "Eight";
+                        break;
+
+                    case 9:
+                        numberWord += "Nine";
                         break;
                 }
+                reversed /= 10;
                 numberWord += " ";
             }
+
+            if (lengNum > lengReversed)
+            {
+                for (int a = 0; a < lengNum - lengReversed; a++)
+                {
+                    numberWord += "Zero ";
+                }
+            }
+
+            System.out.println(numberWord.trim());
         }
     }
 
-    public static int reverseNumber(int number)
+    public static int reverse(int number)
     {
         int reverse = 0;
 
-        while ( number > 0 )
+        if (number < 0 )
         {
-            int digito = number % 10;
+            int tmp = number * -1;
+            while ( tmp > 0 )
+            {
+                int digito = tmp % 10;
 
-            reverse *= 10;
-            reverse += digito;
-            number /= 10;
+                reverse *= 10;
+                reverse += digito;
+                tmp /= 10;
+            }
+            reverse *= -1;
+        }
+        else
+        {
+            while ( number > 0 )
+            {
+                int digito = number % 10;
+
+                reverse *= 10;
+                reverse += digito;
+                number /= 10;
+            }
         }
 
         return reverse;
@@ -60,12 +132,14 @@ public class NumberToWords
         if (number < 0 )
         {
             return -1;
-        }
-        else
+        } else if (number == 0)
+        {
+            return 1;
+        } else
         {
             int digits = 0;
 
-            while (number > 0 )
+            while (number > 0)
             {
                 digits++;
                 number /= 10;
