126e992e65b527b706fcb24f9a2d5434da5f5c20
shiro <shadowfocker666@gmail.com>
2018-06-05 19:43:45 -0500

    Listas Enlazadas 11 - 06 - 2018

---
+++ src/com/intellij/LikedLists/Reproductor.java
@@ -0,0 +1,13 @@
+package com.intellij.LikedLists;
+
+import java.util.ArrayList;
+
+public class Reproductor
+{
+    private ArrayList<Album> albums = new ArrayList<Album>();
+
+    public static void main(String[] args)
+    {
+        Album album = new Album("Stormbringer", "Deep purple");
+    }
+}
