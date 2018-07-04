c5e3ac9ba459e37082667c0ff97e0089a6af9a09
shiro <shadowfocker666@gmail.com>
2018-07-04 16:18:07 -0500

    Coding Exercises

--- src/com/intellij/CodingExercises/PerfectNumber.java
+++ src/com/intellij/CodingExercises/PerfectNumber.java
@@ -2,4 +2,40 @@ package com.intellij.CodingExercises;
 
 public class PerfectNumber
 {
+    public static void main(String[] args)
+    {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+
+    public static boolean isPerfectNumber(int number)
+    {
+        boolean isPerfect = false;
+        int a = 1, sum = 0;
+
+        if (number > 0)
+        {
+            while (a < number)
+            {
+                if (number % a == 0)
+                {
+                    sum += a;
+                }
+                a++;
+            }
+
+            if (number == sum )
+            {
+                isPerfect = true;
+            }
+
+            return isPerfect;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
