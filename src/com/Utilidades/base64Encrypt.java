9051bc769b47c3db40333c2a19c5f2ccc3d02781
shiro <shadowfocker666@gmail.com>
2018-12-13 16:21:25 -0600

    Clases para la manipulacion de xml's actualizacion: Seleccion grafica de archivos filtrados

---
+++ src/com/Utilidades/base64Encrypt.java
@@ -0,0 +1,26 @@
+package com.Utilidades;
+
+import java.util.Base64;
+
+public class base64Encrypt
+{
+    public static void main(String[] args)
+    {
+        String pass = "Contrasen1";
+
+        byte [] encod = Base64.getEncoder().encode(pass.getBytes());
+
+        String stEncod = new String(encod);
+
+        System.out.println("tamaño: " + stEncod.length() + "\nCadena: " + stEncod);
+
+
+        byte [] decod = Base64.getDecoder().decode(encod);
+
+        String stDecod = new String(decod);
+
+        System.out.println("tamaño: " + stDecod.length() + "\nCadena: " + stDecod);
+
+
+    }
+}
