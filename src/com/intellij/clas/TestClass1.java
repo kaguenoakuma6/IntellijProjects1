2fbf80850972c04226bab9becb220c6f6904ad54
kuro <shadowfocker666@gmail.com>
2019-05-10 19:39:31 -0500

    First Upload

--- src/com/intellij/clas/TestClass1.java
+++
@@ -1,9 +0,0 @@
-package com.intellij.clas;
-
-public class TestClass1
-{
-    public static double calculateInterest( double amount, double interestRate)
-    {
-        return ( amount * (interestRate/100));
-    }
-}
